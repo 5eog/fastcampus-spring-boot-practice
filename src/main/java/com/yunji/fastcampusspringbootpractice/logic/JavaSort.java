ea5c53a965c22c0e43b6551f8541678523a7a3d0
yunji <60842443+yunjigo92@users.noreply.github.com>
2022-08-17 12:52:49 +0900

    Add sort logic close #1
    기존 spring 에서 구현한 sort 로직 boot에 적용

---
+++ src/main/java/com/yunji/fastcampusspringbootpractice/logic/JavaSort.java
@@ -0,0 +1,26 @@
+package com.yunji.fastcampusspringbootpractice.logic;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * description
+ * <p>
+ * author         : yunji
+ * date           : 22. 8. 12.
+ */
+@Component
+public class JavaSort <T extends Comparable<T>> implements Sort<T>{
+
+    @Override
+    public List<T> sort(List<T> list){
+        List<T> output = new ArrayList<T>(list);
+
+        Collections.sort(output);
+
+        return output;
+    }
+}
---
+++ src/main/java/com/yunji/fastcampusspringbootpractice/service/SortService.java
@@ -0,0 +1,28 @@
+package com.yunji.fastcampusspringbootpractice.service;
+
+import com.yunji.fastcampusspringbootpractice.logic.Sort;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * description
+ * <p>
+ * author         : yunji
+ * date           : 22. 8. 16.
+ */
+@Service
+public class SortService {
+
+    private final Sort<String> sort;
+
+    public SortService(@Qualifier("javaSort") Sort<String> sort){
+        this.sort =sort;
+        System.out.println("구현체는 " + sort.getClass().getName());
+    }
+
+    public List<String> doSort(List<String> list){
+        return sort.sort(list);
+    }
+}
